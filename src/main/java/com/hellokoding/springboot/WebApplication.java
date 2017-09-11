082ac9bbba04177b45eee419c71cdbf5c9a56aef
qijunbo <junboqi@foxmail.com>
2017-09-11 14:20:35 +0800

    the jsp version.

--- src/main/java/com/hellokoding/springboot/WebApplication.java
+++ src/main/java/com/hellokoding/springboot/WebApplication.java
@@ -1,11 +1,14 @@
 package com.hellokoding.springboot;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
+
 @SpringBootApplication
+@EnableAutoConfiguration
 public class WebApplication extends SpringBootServletInitializer {
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
